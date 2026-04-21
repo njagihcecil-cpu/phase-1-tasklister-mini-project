386b17876f36d20471d77bae4a881e94b324b3ac
njagih <njagih600@gmail.com>
2026-04-21 15:13:41 +0300

    second commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.getElementById('create-task-form');
+const newTask = document.getElementById('new-task-description');
+const tasks = document.getElementById('tasks');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const taskText = newTask.value;
+  
+  if (taskText !== '') {
+    const li = document.createElement('li');
+    const ul = document.getElementById('tasks');
+    li.textContent = taskText;
+
+    li.addEventListener('click', function() {
+      ul.removeChild(li);
+    });
+
+    ul.appendChild(li);
+  }
 });
+
